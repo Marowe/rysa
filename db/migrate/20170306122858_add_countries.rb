a525568257ff256db04c6a0fd424617f48b10f03
Type in your name <Type in your email>
2017-03-06 13:33:34 +0100

    add gem pry and adding countries migration

---
+++ db/migrate/20170306122858_add_countries.rb
@@ -0,0 +1,33 @@
+class AddCountries < ActiveRecord::Migration[5.0]
+  class Country < ApplicationRecord
+     belongs_to :target_group
+  end
+
+  def up
+    countries_attributes.each do |country|
+      Country.create! country
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+
+  private
+
+  def countries_attributes
+    [
+      {
+         country_code: "PL"
+         panel_provider_id: 1
+      },
+      {
+         country_code: "RU"
+         panel_provider_id: 2
+      },
+      {
+         country_code: "AU"
+         panel_provider_id: 3
+      },
+  ]
+end
