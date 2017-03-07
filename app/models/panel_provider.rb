a82ca6bb8244340173c21099e49b1eb4540ad00d
Type in your name <Type in your email>
2017-03-07 14:40:18 +0100

    add gem act_as_tree, add locations and locations_groups migration

--- app/models/panel_provider.rb
+++ app/models/panel_provider.rb
@@ -1,2 +1,3 @@
 class PanelProvider < ApplicationRecord
+acts_as_tree
 end
---
+++ db/migrate/20170307131900_add_location_groups.rb
@@ -0,0 +1,36 @@
+class AddLocationGroups < ActiveRecord::Migration[5.0]
+  class LocationGroups < ApplicationRecord
+  end
+
+  def up
+    location_groups_attributes.each do |location_group|
+      LocationGroup.create! location_group
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+
+  private
+
+  def location_groups_attributes
+    [
+      {
+        country_id: 1,
+        panel_provider_id: 1
+      },
+      {
+        country_id: 2,
+        panel_provider_id: 2
+      },
+      {
+        country_id: 3,
+        panel_provider_id: 3
+      },
+      {
+        country_id: 4
+      }
+    ]
+  end
+end
