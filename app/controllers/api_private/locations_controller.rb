3d5611e922920bc1e25f4d991dd8d075e22afd5f
Type in your name <Type in your email>
2017-03-20 17:31:28 +0100

    correction code in controllers

--- app/controllers/api_private/locations_controller.rb
+++ app/controllers/api_private/locations_controller.rb
@@ -2,9 +2,8 @@ class ApiPrivate::LocationsController < ApplicationController
   def index
     country = Country.find_by_country_code(params[:country_code])
     if country
-      location = Location.where(location_group: country.location_group_id)
-      locations = location.pluck(:name)
-      render json: {locations: locations}
+      location_names = Location.where(location_group: country.location_group_id).pluck(:name)
+      render json: {locations: location_names}
     else
       render json: { error: "Ups! There is no country!", available_params: "/PL, /RU, /AU"}, status: 400
     end
--- app/controllers/api_private/target_groups_controller.rb
+++ app/controllers/api_private/target_groups_controller.rb
@@ -2,8 +2,7 @@ class ApiPrivate::TargetGroupsController < ApplicationController
   def index
     country = Country.find_by_country_code(params[:country_code])
     if country
-      target_group = TargetGroup.where(panel_provider_id: country.panel_provider_id)
-      target_groups = target_group.pluck(:id)
+      target_groups = TargetGroup.where(panel_provider_id: country.panel_provider_id).pluck(:id)
       render json: {TargetGroups: target_groups}
     else
       render json: { error: "Ups! There is no country!", available_params: "/PL, /RU, /AU"}, status: 400
